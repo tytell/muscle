5adc6daa9651c11ca90e87293c0d8ed04b63f641
Eric Tytell <tytell@jhu.edu>
2012-06-06 16:19:22 -0400

    Function for checking an analytical Jacobian against the numerical one

---
+++ check_jacobian.m
@@ -0,0 +1,23 @@
+function check_jacobian(t,X,dx, statefcn,jacobianfcn)
+
+Jn = zeros(length(X), length(X));
+
+for i = 1:length(X)
+    X1 = X;
+    X1(i) = X1(i) + dx(i);
+    Yp = statefcn(t,X1);
+    
+    X1 = X;
+    X1(i) = X1(i) - dx(i);
+    Ym = statefcn(t,X1);
+    
+    dY1 = (Yp - Ym) / (2*dx(i));
+    
+    Jn(:,i) = dY1;
+end
+
+Ja = jacobianfcn(t,X);
+
+fprintf('Norm of difference in analytical and numerical Jacobian: %f\n', norm(Ja-Jn));
+fprintf('Difference matrix:\n');
+disp(Ja-Jn);
